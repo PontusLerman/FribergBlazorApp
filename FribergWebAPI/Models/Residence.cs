e09b3d3af58ed8cfc1f01264f9d29dbb3a8eee49
Christian Alp <Chrillealp11@hotmail.com>
2024-04-11 14:28:53 +0200

    FIK-6
    Added Data and Model folders.
    Added Residence Model.

---
+++ FribergWebAPI/Models/Residence.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FribergWebAPI.Models
+{
+    public class Residence
+    {
+        public int Id { get; set; }
+        public string? Category { get; set; }
+        public string? Address { get; set; }
+        public string? Municipality { get; set; }
+        public decimal StartingPrice { get; set; }
+        public decimal LivingArea { get; set; }
+        public decimal Biarea { get; set; }
+        public decimal PlotArea { get; set; }
+        public string? ObjectDescription { get; set; }
+        public int NumberOfRooms { get; set; }
+        public decimal MonthlyFee { get; set; }
+        public decimal OperatingCostPerYear { get; set; }
+        public int ConstructionYear { get; set; }
+    }
+}
