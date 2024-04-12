303cd7f1cc42bcd5d2fb5fbd01cf498767d074aa
Johan <krangh@gmail.com>
2024-04-12 07:42:25 +0200

    Added Agency class

---
+++ FribergWebAPI/Models/Agency.cs
@@ -0,0 +1,9 @@
+﻿namespace FribergWebAPI.Models
+{
+    public class Agency
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LogoURL { get; set; } = "";
+    }
+}
