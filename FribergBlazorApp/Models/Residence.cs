127dcf8a8e160e4b8374a7f375c363771440a2c1
Johan <krangh@gmail.com>
2024-04-19 09:14:51 +0200

    Added models to Blazor

---
+++ FribergBlazorApp/Models/Residence.cs
@@ -0,0 +1,23 @@
+﻿namespace FribergBlazorApp.Models
+{
+
+    public class Residence
+    {
+        public int id { get; set; }
+        public Category category { get; set; }
+        public string address { get; set; }
+        public Municipality municipality { get; set; }
+        public int startingPrice { get; set; }
+        public int livingArea { get; set; }
+        public int biArea { get; set; }
+        public int plotArea { get; set; }
+        public string objectDescription { get; set; }
+        public int numberOfRooms { get; set; }
+        public int monthlyFee { get; set; }
+        public int operatingCostPerYear { get; set; }
+        public int constructionYear { get; set; }
+        public Picture[] pictures { get; set; }
+        public Realtor realtor { get; set; }
+    }    
+
+}
