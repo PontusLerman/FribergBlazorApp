0ff667deedc121ab45d8d58da83e1def21a76d55
PontusLerman <pontuslerman@gmail.com>
2024-04-26 15:08:53 +0200

    Modified Agency model and added pages for Crud.

--- FribergBlazorApp/Models/Agency.cs
+++ FribergBlazorApp/Models/Agency.cs
@@ -5,8 +5,7 @@
         public int agencyId { get; set; }
         public string agencyName { get; set; }
         public string agencyDescription { get; set; }
-        public string agencyLogoURL { get; set; }
-        public string[] employees { get; set; }
+        public string agencyLogoURL { get; set; }     
+        //public string[] employees { get; set; }
     }
-
 }
