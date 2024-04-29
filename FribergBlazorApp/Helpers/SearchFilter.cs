506c125889bc52e1c17eb372a33d88555471abe9
Christian Alp <Chrillealp11@hotmail.com>
2024-04-29 14:20:41 +0200

    Added a List to the search filter
    You can now search for more than one entity for example you can search for both the ID and the name of a category.

--- FribergBlazorApp/Helpers/SearchFilter.cs
+++ FribergBlazorApp/Helpers/SearchFilter.cs
@@ -9,45 +9,43 @@ namespace FribergBlazorApp.Helpers
 	{
 		private readonly IEnumerable<T> _sourceList;
 		private IEnumerable<T> _filteredItems;
-		private Func<T, string> _propertySelector;
+		private List<Func<T, string>> _propertySelectors;
 		private string _searchTerm;
 
-		public string SearchTerm
-		{
-			get { return _searchTerm; }
-			set
-			{
-				_searchTerm = value;
-				ApplyFilter();
-			}
-		}
+		public string SearchTerm { get; set;}
 
-		public SearchFilter(IEnumerable<T> sourceList, Func<T, string> propertySelector)
+		public SearchFilter(IEnumerable<T> sourceList, params Func<T, string>[] propertySelectors)
 		{
 			_sourceList = sourceList;
-			_propertySelector = propertySelector;
+			_propertySelectors = propertySelectors.ToList();
 			_filteredItems = _sourceList;
 			_searchTerm = string.Empty;
 		}
 
-		public IEnumerable<T> FilteredItems => _filteredItems;
+		public IEnumerable<T> ApplyFilter()
+        {
+            if (string.IsNullOrWhiteSpace(SearchTerm))
+            {
+                _filteredItems = _sourceList;
+            }
+            else
+            {
+                _filteredItems = _sourceList.Where(IsMatch);
+            }
+            return _filteredItems;
+        }
 
-		private void ApplyFilter()
-		{
-			if (string.IsNullOrWhiteSpace(_searchTerm))
-			{
-				_filteredItems = _sourceList;
-			}
-			else
-			{
-				_filteredItems = _sourceList.Where(item => IsMatch(item));
-			}
-		}
-
-		private bool IsMatch(T item)
-		{
-			var propertyValue = _propertySelector(item);
-			return propertyValue?.IndexOf(_searchTerm, StringComparison.OrdinalIgnoreCase) >= 0;
-		}
+        private bool IsMatch(T item)
+        {
+            foreach (var propertySelector in _propertySelectors)
+            {
+                var propertyValue = propertySelector(item);
+                if (propertyValue?.IndexOf(SearchTerm, StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 	}
 }
