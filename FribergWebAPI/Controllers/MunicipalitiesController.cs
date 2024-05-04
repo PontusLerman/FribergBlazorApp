5172c932096fd15363dd8e699f6919526f4822e4
Johan <147413295+jkrangh@users.noreply.github.com>
2024-05-04 21:11:56 +0200

    Change MunicipalityController to use Dtos.

--- FribergWebAPI/Controllers/MunicipalitiesController.cs
+++ FribergWebAPI/Controllers/MunicipalitiesController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using FribergWebAPI.Models;
 using FribergWebAPI.Data.Interfaces;
 using AutoMapper;
+using FribergWebAPI.DTOs;
 
 namespace FribergWebAPI.Controllers
 {
@@ -26,36 +27,38 @@ namespace FribergWebAPI.Controllers
 
         // GET: api/Municipalities
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Municipality>>> GetMunicipality()
+        public async Task<ActionResult<IEnumerable<MunicipalityDto>>> GetMunicipality()
         {
             var municipalities = await municipalityRepo.GetAllAsync();
-            return Ok(municipalities);
+            var municipalityDtos = mapper.Map<List<MunicipalityDto>>(municipalities);
+            return Ok(municipalityDtos);
         }
 
         // GET: api/Municipalities/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Municipality>> GetMunicipality(int id)
+        public async Task<ActionResult<MunicipalityDto>> GetMunicipality(int id)
         {
             var municipality = await municipalityRepo.GetByIdAsync(id);
-
+            var municipalityDto = mapper.Map<MunicipalityDto>(municipality);
             if (municipality == null)
             {
                 return NotFound();
             }
 
-            return municipality;
+            return municipalityDto;
         }
 
         // PUT: api/Municipalities/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutMunicipality(int id, Municipality municipality)
+        public async Task<IActionResult> PutMunicipality(int id, MunicipalityDto municipalityDto)
         {
-            if (id != municipality.Id)
+            if (id != municipalityDto.Id)
             {
                 return BadRequest();
             }
 
+            var municipality = mapper.Map<Municipality>(municipalityDto);
             await municipalityRepo.UpdateAsync(municipality);
 
             return NoContent();
@@ -64,8 +67,9 @@ namespace FribergWebAPI.Controllers
         // POST: api/Municipalities
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Municipality>> PostMunicipality(Municipality municipality)
+        public async Task<ActionResult<Municipality>> PostMunicipality(MunicipalityDto municipalityDto)
         {
+            var municipality = mapper.Map<Municipality>(municipalityDto);
             await municipalityRepo.AddAsync(municipality);
 
             return CreatedAtAction("GetMunicipality", new { id = municipality.Id }, municipality);
