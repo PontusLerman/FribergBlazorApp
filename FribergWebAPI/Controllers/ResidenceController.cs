e81011f90b55df56be3de5691cd020a4127ee0a3
Christian Alp <Chrillealp11@hotmail.com>
2024-05-17 14:08:01 +0200

    Update error messages

--- FribergWebAPI/Controllers/ResidenceController.cs
+++ FribergWebAPI/Controllers/ResidenceController.cs
@@ -65,7 +65,6 @@ namespace FribergWebAPI.Controllers
 				return BadRequest();
 			}
 
-		//This here or up there?	//var residence = mapper.Map<Residence>(residenceDto);
 			await residenceRepository.Update(residence);
 			return NoContent();
 		}
