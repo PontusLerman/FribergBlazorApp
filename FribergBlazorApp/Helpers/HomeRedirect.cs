05db94efd61d2d51c2f74c33d7bf679138e9a49c
PontusLerman <pontuslerman@gmail.com>
2024-05-20 20:48:48 +0200

    Added HomeRedirect helper. Applyed it in NewRealtorIndex.

---
+++ FribergBlazorApp/Helpers/HomeRedirect.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace FribergBlazorApp.Helpers
+{
+    public class HomeRedirect
+    {
+        private readonly NavigationManager navigationManager;
+        private readonly AuthenticationStateProvider authenticationStateProvider;
+
+        public HomeRedirect(NavigationManager navigationManager, AuthenticationStateProvider authenticationStateProvider)
+        {
+            this.navigationManager = navigationManager;
+            this.authenticationStateProvider = authenticationStateProvider;
+        }
+
+        public void CheckIfSuperRealtor(ClaimsPrincipal user)
+        {
+            if (!user.IsInRole("SuperRealtor"))
+            {
+                navigationManager.NavigateTo("/");
+            }
+        }
+        public void CheckIfDefaultRealtorOrSuperRealtor(ClaimsPrincipal user)
+        {
+            if (!user.IsInRole("DefaultRealtor") && !user.IsInRole("SuperRealtor"))
+            {
+                navigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
