dce85744a12f9975b39d960818fe9bf7f13d9d3d
Johan <147413295+jkrangh@users.noreply.github.com>
2024-05-20 11:15:18 +0200

    Built and added Overlay-service

---
+++ FribergBlazorApp/Services/OverlayService.cs
@@ -0,0 +1,29 @@
+﻿//author: Johan Krångh
+namespace FribergBlazorApp.Services
+{
+    public class OverlayService : IOverlayService
+    {
+        private bool isVisible;
+
+        private string imageUrl;
+
+        public bool IsVisible => isVisible;
+        public string ImageUrl => imageUrl;
+
+        public event Action OnChange;
+
+        public void HideImage()
+        {
+            isVisible = false;
+            NotifyStateChange();
+        }
+
+        public void ShowImage(string imageUrl)
+        {
+            this.imageUrl = imageUrl;
+            isVisible = true;
+            NotifyStateChange();
+        }
+        private void NotifyStateChange() => OnChange?.Invoke();
+    }
+}
